a741ecffdb3847a6e41c9aafb7334c337226c601
Your name <omowunmidaud1@gmail.com>
2023-11-25 04:55:53 +0100

    completed the home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import logo from '../images/logo.png'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+
+      <section className='foot-top'>
+
+        <div className='foot-top-left'>
+
+          <img src={logo} alt='logo' id='logo2' />
+          <p>Enhancing remote exams integrity</p>
+          <div className='icons'>
+            <ion-icon name="logo-whatsapp"></ion-icon>
+            <ion-icon name="call-outline"></ion-icon>
+            <ion-icon name="mail-outline"></ion-icon>
+          </div>
+
+        </div>
+
+        <div className='foot-top-left'>
+          
+          <Link to='/register' id='link1'>Register</Link>
+
+        </div>
+
+      </section>
+
+      <section className='foot-bottom'>
+
+          <p>&copy; SDproctor 2023</p>
+
+          <div className='foot-nav'>
+
+            <Link to='/' className='link1'>Home</Link>
+            <Link to='/' className='link1'>Why SD Proctor</Link>
+            <Link to='/' className='link1'>About</Link>
+            <Link to='/register' className='link1'>Register</Link>
+            <Link to='/' className='link1'>Contact</Link>
+            
+          </div>
+      </section>
+
+    </footer>
   )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css';
 import { Navbar } from './Navbar'
 import image from '../images/Editing body text-rafiki 1.png'
 import image1 from '../images/Group 72.png'
@@ -8,7 +9,12 @@ import image4 from '../images/Group 73.png'
 import image5 from '../images/carbon_idea.png'
 import image6 from '../images/healthicons_health-data-security-outline.png'
 import image7 from '../images/efficiency-svgrepo-com 2.png'
+import about from '../images/About us page-rafiki 2.png'
+import contact from '../images/istockphoto-1178752391-612x612-removebg-preview 1.png'
+import circle from '../images/Ellipse 5.png'
+import circle1 from '../images/Ellipse 6.png'
 import { Card } from './Card'
+import { Footer } from './Footer';
 
 const Home = () => {
   return (
@@ -86,6 +92,97 @@ const Home = () => {
             />
 
         </section>
+
+        <section id='aboutsd'>
+
+            <h2>About</h2>
+            <p>SDproctor is a website mainly concerned about  enhancing the security of online examinations,
+                we are also conerened about the privacy and  data security, of our verified exam takers which includes:
+            </p>
+
+        </section>
+        
+        <section className='features'>
+
+            <article id='about-article'>
+
+                <div className='about-content'>
+
+                    <img src={circle} alt='' />
+                    <p>EnsurIng the security by implementing protective measures, like encryption, user authentication and secure data transmission. 
+                        These measures will safeguard both user data and the integrity of proctoring sessions
+                    </p>
+
+                </div>
+
+                <div className='about-content'>
+
+                    <img src={circle1} alt='' />
+                    <p>Create a web-based interface that is easy to use and intuitively allow test takers, proctors and administrators to interact smoothly within the proctoring system.</p>
+
+                </div>
+
+                <div className='about-content'>
+
+                    <img src={circle} alt='' />
+                    <p>Developing backend features that facilitate the proctoring process, 
+                        such, as real time monitoring, secure identity verification and incident reporting. These functionalities ensure the integrity of assessments.
+                    </p>
+
+                </div>
+
+                <div className='about-content'>
+
+                    <img src={circle1} alt='' />
+                    <p>To guarantee the safety of our system we will incorporate measures 
+                        such, as encryption, user authentication and secure data transmission. These precautions are put in place to protect user data and maintain the integrity of proctoring sessions
+                    </p>
+
+                </div>
+
+            </article>
+
+            <img src={about} alt='' />
+
+        </section>
+
+        <section className='contact'>
+
+            <h2>Contact</h2>
+
+        </section>
+        
+        <section className='features1'>
+
+            <img src={contact} alt='' />
+
+            <article id='contact-article'>
+
+                <div className='contact-content'>
+
+                    <p className='p1'>We are available to be of help!</p>
+                    <p className='p2'>Have any question to ask us?</p>
+                    <p className='p3'>kindly do well to reach out to us through</p>
+                    <p className='p3'>our Email / phone no below;</p>
+                    <p className='p4'>SDproctor@gmail.com / 07067882505</p>
+
+                </div>
+
+                <div className='contact-content'>
+
+                    <p className='p1'>You can as well be of help to us!</p>
+                    <p className='p2'>Wondering how? yes it soo easy, we will be needing your help to reachout to us on geniu ways to 
+                        improve our services and feebacks on how beneficial our website has been to you using the Email below;
+                    </p>
+                    <p className='p4'>08067284588 / SDproctor78@gmail.com</p>
+
+                </div>
+
+            </article>
+            
+        </section>
+
+        <Footer />
 
     </>
   )
